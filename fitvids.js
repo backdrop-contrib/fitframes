092903541dfa6b6aad3e4a6051698bf5677a07aa
Derek Ahmedzai <derek@sharpshooter.org>
2013-07-10 11:36:46 +0100

    Removed console.log

--- fitvids.js
+++ fitvids.js
@@ -5,7 +5,6 @@
     // Get settings for this behaviour
     var selectors = settings.fitvids['selectors'];
     var simplifymarkup = settings.fitvids['simplifymarkup'];
-    console.log(simplifymarkup);
     var custom_domains = settings.fitvids['custom_domains'].join();
         
     // Remove media wrappers
@@ -15,14 +14,12 @@
         $(".media-youtube-preview-wrapper").removeAttr("style");
         $(".media-youtube-outer-wrapper").removeClass("media-youtube-outer-wrapper");
         $(".media-youtube-preview-wrapper").removeClass("media-youtube-preview-wrapper");
-        // console.log('Removed .media-youtube... wrappers');
       }
       if ($(".media-vimeo-outer-wrapper").length) {
         $(".media-vimeo-outer-wrapper").removeAttr("style");
         $(".media-vimeo-preview-wrapper").removeAttr("style");
         $(".media-vimeo-outer-wrapper").removeClass("media-vimeo-outer-wrapper");
         $(".media-vimeo-preview-wrapper").removeClass("media-vimeo-preview-wrapper");
-        // console.log('Removed .media-vimeo... wrappers');
       }
     }
     
